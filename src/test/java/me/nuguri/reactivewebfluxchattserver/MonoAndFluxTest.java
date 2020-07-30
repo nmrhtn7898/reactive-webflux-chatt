26f0dcb0082bb511589e5e35b9d3e332bf6ed9ba
SYJ AT HOME <nmrhtn7898@naver.com>
2020-07-30 22:26:21 +0900

    add hotflux

--- src/test/java/me/nuguri/reactivewebfluxchattserver/MonoAndFluxTest.java
+++ src/test/java/me/nuguri/reactivewebfluxchattserver/MonoAndFluxTest.java
@@ -153,6 +153,44 @@ public class MonoAndFluxTest {
         countDownLatch.await(2, TimeUnit.SECONDS); // main 쓰레드가 데몬 쓰레드의 동작 종료를 기다려주는 타임아웃 세컨드 설정
     }
 
+    @Test
+    void hotFlux() {
+        CountDownLatch countDownLatch = new CountDownLatch(2); // 최대 2개의 쓰레드 동시 수행
+        final List<String> basket1 = asList("kiwi", "orange", "lemon", "orange", "lemon", "kiwi");
+        final List<String> basket2 = asList("banana", "lemon", "lemon", "kiwi");
+        final List<String> basket3 = asList("strawberry", "orange", "lemon", "grape", "strawberry");
+        final List<List<String>> baskets = asList(basket1, basket2, basket3);
+        final Flux<List<String>> basketFlux = Flux.fromIterable(baskets);
+        basketFlux.concatMap(basket -> {
+            final Flux<String> source = Flux.fromIterable(basket).log().publish().autoConnect(2);
+            final Mono<List<String>> distinctFruits = source.distinct().collectList();
+            final Mono<Map<String, Long>> countFruitsMono = source
+                    .groupBy(fruit -> fruit) // 바구니로 부터 넘어온 과일 기준으로 group을 묶는다.
+                    .concatMap(groupedFlux -> groupedFlux.count()
+                            .map(count -> {
+                                final Map<String, Long> fruitCount = new LinkedHashMap<>();
+                                fruitCount.put(groupedFlux.key(), count);
+                                return fruitCount;
+                            }) // 각 과일별로 개수를 Map으로 리턴
+                    ) // concatMap으로 순서보장
+                    .reduce((accumulatedMap, currentMap) -> new LinkedHashMap<String, Long>() { {
+                        putAll(accumulatedMap);
+                        putAll(currentMap);
+                    }}); // 그동안 누적된 accumulatedMap에 현재 넘어오는 currentMap을 합쳐서 새로운 Map을 만든다. // map끼리 putAll하여 하나의 Map으로 만든다.
+            return Flux.zip(distinctFruits, countFruitsMono, (distinct, count) -> new FruitInfo(distinct, count));
+        }).subscribe(
+                System.out::println,  // 값이 넘어올 때 호출 됨, onNext(T)
+                error -> {
+                    System.err.println(error);
+                    countDownLatch.countDown();
+                }, // 에러 발생시 출력하고 countDown, onError(Throwable)
+                () -> {
+                    System.out.println("complete");
+                    countDownLatch.countDown();
+                } // 정상적 종료시 countDown, onComplete()
+        );
+    }
+
 
     @ToString
     @RequiredArgsConstructor
